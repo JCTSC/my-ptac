b06e70c32ba685213a2ad12cec2a3224cb356459
JCTSC <jctscamargo@gmail.com>
2024-09-26 15:46:14 -0300

    Comitting the button
    Buttón 26.09

---
+++ src/app/components/botao.tsx
@@ -0,0 +1,31 @@
+type ButtonProp = {
+    name: string
+    numero: number
+}
+
+const Button: React.FC<ButtonProp> = ({name,numero}) => {
+    return <div><h1>(name) oi (numero)</h1></div>
+}
+
+export default Button
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
