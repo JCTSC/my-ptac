ad8a101a9bb521fa49d09d66a1cf8b6eaf89af23
JCTSC <jctscamargo@gmail.com>
2024-11-07 14:54:03 -0400

    Update page.tsx

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -4,67 +4,56 @@ import { FormEvent, useEffect, useState } from "react";
 import { useRouter } from "next/navigation";
 import Usuario from "../interface/usuario";
 import Link from "next/link";
-import { ApiUrl } from "../config"
-
+import { ApiURL } from "../config"
 
 
   export default function Login() {
   const [email, setEmail] = useState<string>('');
-  const [senha, setSenha] = useState<string>('');
+  const [password, setPassword] = useState<string>('');
   const [error, setError] = useState<string>('');
 
 
 
 
-
-
-
-
-
-
-
-
-
-
-  const [usuarios, setUsuarios] = useState<Usuario[]>([
-    {
-      id: 1,
-      nome: "Davi Kazan",
-      email: "davirevoltado@gmail.com",
-      senha: "senha123",
-      tipo: "adm"
-    },
-    {
-      id: 2,
-      nome: "Ghost Arashi",
-      email: "ghostaashi@gmail.com",
-      senha: "senha123",
-      tipo: "cliente"
+  interface ResponseSignin {
+    erro: boolean,
+    mensagem: string,
+    token?: string
+  }
+  
+  
+  
+      const  handleSubmit = async (e : FormEvent) => {
+        e.preventDefault();
+        try {
+         const response = await fetch(`${ApiURL}/auth/login`, {
+          method: 'POST',
+          headers: {
+            'Content-Type' : 'application/json'
+          },
+          body: JSON.stringify({email, password})
+         })
+          if (response){
+            const data : ResponseSignin = await response.json()
+            const {erro, mensagem, token = ''} = data;
+            console.log(data)
+            if (erro){
+              setError(mensagem)
+            } else {
+              // npm i nookies setCookie
+              setCookie(undefined, 'restaurant-token', token, {
+                maxAge: 60*60*1 // 1 hora
+              } )
+  
+            }
+          } else {
+  
+          }
+      } 
+       catch (error) {
+      console.error('Erro na requisicao', error)
     }
-  ])
-
-
-
-
-
-
-  const handleSubmit = async (e:FormEvent) => {
-    e.preventDefault();
-
-    const response = await fetch(`${ApiURL}/auth/login`, {
-      method: "POST",
-      headers: {
-        'Content-Type' : ' application/json'
-      },
-      body: JSON.stringify({email, password})
-    })
-
-
-
-
-
-
-
+  
 
 
 
@@ -73,12 +62,12 @@ const router = useRouter();
 
   const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault()
-    const usuario = usuarios.find((user) => user.email == email && user.password == senha)
+    const usuario = usuarios.find((user) => user.email == email && user.password == password)
     if(usuario){
       localStorage.setItem('usuario', JSON.stringify(usuario))
       router.push('/home')
     } else {
-      setError('Email ou senha inválido!')
+      setError('Email ou password inválido!')
     }
   }
 
@@ -96,8 +85,8 @@ const router = useRouter();
 
   function verificarLogin(e: React.FormEvent) {
     e.preventDefault();
-    if (email !== 'ptac4' || senha !== 'nota10') {
-      setError('E-mail ou senha inválidos');
+    if (email !== 'ptac4' || password !== 'nota10') {
+      setError('E-mail ou password inválidos');
       return;
     }
 
